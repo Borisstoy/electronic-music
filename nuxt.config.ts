60699d3cb8a5e2dd4bcb1220b12e93f895e3fb0f
Borisstoy <boris.stoy@gmail.com>
2020-11-04 09:34:53 -0500

    Migrate ts config to typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
+import { NuxtConfig } from '@nuxt/types'
 
-export default {
+const config: NuxtConfig = {
+  // Define your configuration with auto-completion & type checking
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - electronic-music',
@@ -12,16 +14,16 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
+  
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-
+  
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
-
+  
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
+  
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
@@ -29,7 +31,7 @@ export default {
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
   ],
-
+  
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
@@ -37,10 +39,10 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
-
+  
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
-
+  
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
@@ -59,7 +61,9 @@ export default {
       },
     },
   },
-
+  
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
+
+export default config
